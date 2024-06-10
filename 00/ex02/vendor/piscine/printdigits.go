3466299a3367ce21df107462bd13532616da531f
naopen <naokizssm@gmail.com>
2024-06-10 17:48:58 +0900

    Add: 00/ex02

---
+++ 00/ex02/vendor/piscine/printdigits.go
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printdigits.go                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 17:47:02 by nkannan           #+#    #+#             */
+/*   Updated: 2024/06/10 17:47:50 by nkannan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+package piscine
+
+import "ft"
+
+// PrintDigits prints the digits from 0 to 9.
+func PrintDigits() {
+	for i := '0'; i <= '9'; i++ {
+		ft.PrintRune(i)
+	}
+	ft.PrintRune('\n')
+}
