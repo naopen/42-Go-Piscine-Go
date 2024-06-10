9b85c22aba1eaf75555213423d1614642baf588b
naopen <naokizssm@gmail.com>
2024-06-11 05:36:40 +0900

    Add: 01/ex00

---
+++ 01/ex00/vendor/piscine/pointone.go
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pointone.go                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 05:31:28 by nkannan           #+#    #+#             */
+/*   Updated: 2024/06/11 05:33:37 by nkannan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+package piscine
+
+// PointOne takes a pointer to an int and sets the value to 1.
+func PointOne(nb *int) {
+	*nb = 1
+}
