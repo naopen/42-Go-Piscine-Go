fca32edb6ff12f21644502921ca07382d0d3f113
naopen <naokizssm@gmail.com>
2024-06-25 01:54:56 +0900

    Update strrev function to reverse string efficiently

--- 01/ex07/vendor/piscine/strrev.go
+++ 01/ex07/vendor/piscine/strrev.go
@@ -6,7 +6,7 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 17:01:06 by nkannan           #+#    #+#             */
-/*   Updated: 2024/06/11 17:02:12 by nkannan          ###   ########.fr       */
+/*   Updated: 2024/06/25 01:54:02 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,9 @@ func StrLen(str string) int {
 // 2. Convert the rune slice to a string manually.
 func StrRev(s string) string {
 	length := StrLen(s)
-
-	reversedRunes := make([]rune, length)
-	index := length - 1
-	for _, r := range s {
-		reversedRunes[index] = r
-		index--
+	var reversed string
+	for i := length - 1; i >= 0; i-- {
+		reversed += string(s[i])
 	}
-
-	reversed := ""
-	for i := 0; i < length; i++ {
-		reversed += string(reversedRunes[i])
-	}
-
 	return reversed
 }
