3ffcc2a325afa0d71d6d2182d432085c2d71b47e
naopen <naokizssm@gmail.com>
2024-07-01 01:08:02 +0900

    Update PrintProgramName to return program name without path

--- 04/ex00/vendor/piscine/printprogramname.go
+++ 04/ex00/vendor/piscine/printprogramname.go
@@ -6,7 +6,7 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 23:41:12 by nkannan           #+#    #+#             */
-/*   Updated: 2024/07/01 00:32:39 by nkannan          ###   ########.fr       */
+/*   Updated: 2024/07/01 01:06:28 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,5 @@ import "os"
 
 // PrintProgramName returns the name of the program without the path.
 func PrintProgramName() string {
-	fullPath := os.Args[0]
-	programName := ""
-
-	// Find the last slash in the fullPath
-	for i := range fullPath {
-		if fullPath[i] == '/' {
-			programName = ""
-		} else {
-			programName += string(fullPath[i])
-		}
-	}
-
-	return programName
+	return os.Args[0][2:]
 }
