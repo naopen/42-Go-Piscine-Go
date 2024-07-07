388d0d86b1762d9afaafd96958657db2ace2b2c8
naopen <naokizssm@gmail.com>
2024-07-08 00:58:27 +0900

    Add: 09/ex01

---
+++ 09/ex01/main.go
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.go                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 00:57:37 by nkannan           #+#    #+#             */
+/*   Updated: 2024/07/08 00:57:43 by nkannan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	link := &piscine.List{}
+
+	piscine.ListPushFront(link, "Hello")
+	piscine.ListPushFront(link, "there")
+	piscine.ListPushFront(link, "how are you")
+
+	it := link.Head
+	for it != nil {
+		fmt.Print(it.Data, " ")
+		it = it.Next
+	}
+	fmt.Println()
+}
